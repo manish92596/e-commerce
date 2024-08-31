1e86d3b8fbb89a07d37bd8de3392e32a4aabaf93
manish92596 <manishkumar92596@gmail.com>
2024-09-01 02:00:11 +0530

    dis

--- e-commerce.py
+++ e-commerce.py
@@ -145,6 +145,20 @@ def search():
     return jsonify(results)
 
 
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('q', '')
+    db = get_db()
+    cursor = db.cursor()
+
+    # Vulnerable code: directly injecting the user input into the SQL query
+    sql_query = f"SELECT * FROM products WHERE name LIKE '%{query}%'"
+    cursor.execute(sql_query)
+
+    results = cursor.fetchall()
+    return jsonify(results)
+
+
 
 # @app.route('/checkout', methods=['POST'])
 # def checkout():
