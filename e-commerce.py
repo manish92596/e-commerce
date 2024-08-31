bee1f9c64d77eb45f439b7fedc38b2a2740b3baf
manish92596 <manishkumar92596@gmail.com>
2024-09-01 03:10:18 +0530

    neww

--- e-commerce.py
+++ e-commerce.py
@@ -226,12 +226,12 @@ def unsafe_load():
     obj = pickle.loads(content)
     return jsonify({"message": "Object loaded successfully!", "object_details": str(obj)})
 
-# @app.route('/process-payment', methods=['POST'])
-# def process_payment():
-#     card_number = request.json.get('card_number')
-#     amount = request.json.get('amount')
-#     result = process_credit_card(card_number, amount)
-#     return jsonify(result)
+@app.route('/process-payment', methods=['POST'])
+def process_payment():
+    card_number = request.json.get('card_number')
+    amount = request.json.get('amount')
+    result = process_credit_card(card_number, amount)
+    return jsonify(result)
 
 
 import requests
