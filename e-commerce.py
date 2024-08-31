b54f6dae3268f0ac079def83b84cd16e6e651558
manish92596 <manishkumar92596@gmail.com>
2024-08-31 19:22:07 +0530

    admin

--- e-commerce.py
+++ e-commerce.py
@@ -69,14 +69,14 @@ def order():
     db.commit()
     return jsonify({"message": "Order placed successfully!"}), 201
 
-@app.route('/admin', methods=['GET'])
-def admin():
-    db = get_db()
-    orders = db.execute('''SELECT o.id, u.username, p.name 
-                           FROM orders om
-                           JOIN users u ON o.user_id = u.id 
-                           JOIN products p ON o.product_id = p.id''').fetchall()
-    return jsonify([{'order_id': row[0], 'username': row[1], 'product_name': row[2]} for row in orders])
+# @app.route('/admin', methods=['GET'])
+# def admin():
+#     db = get_db()
+#     orders = db.execute('''SELECT o.id, u.username, p.name 
+#                            FROM orders om
+#                            JOIN users u ON o.user_id = u.id 
+#                            JOIN products p ON o.product_id = p.id''').fetchall()
+#     return jsonify([{'order_id': row[0], 'username': row[1], 'product_name': row[2]} for row in orders])
 
 
 # @app.route('/data_admin', methods=['GET'])
