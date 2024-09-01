74c48b0d8afcb0a10e26b9c7696b4f68950e250a
manish92596 <manishkumar92596@gmail.com>
2024-09-01 10:39:06 +0530

    newadd

--- e-commerce.py
+++ e-commerce.py
@@ -14,27 +14,17 @@ def redirect_to_external_api():
         return response.text
     except Exception as e:
         return str(e), 500
-    
-@app.route('/redirect', methods=['POST'])
-def redirect_to_external_api():
-    external_api_url = request.json.get('url')
-    try:
-        response = requests.get(external_api_url)
-        return response.text
-    except Exception as e:
-        return str(e), 500
-    
 
 
-@app.route('/redirect', methods=['POST'])
-def redirect_to_external_api():
-    external_api_url = request.json.get('url')
-    try:
-        response = requests.get(external_api_url)
-        return response.text
-    except Exception as e:
-        return str(e), 500
-    
+@app.route('/admin', methods=['GET'])
+def admin():
+    db = get_db()
+    orders = db.execute('''SELECT o.id, u.username, p.name 
+                           FROM orders om
+                           JOIN users u ON o.user_id = u.id 
+                           JOIN products p ON o.product_id = p.id''').fetchall()
+    return jsonify([{'order_id': row[0], 'username': row[1], 'product_name': row[2]} for row in orders])
+
 
 # @app.route('/dummy', methods=['POST'])
 # def dummy():
