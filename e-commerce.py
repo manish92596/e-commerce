7c784842a77e8afefc6e70199dd01e985a134321
manish92596 <manishkumar92596@gmail.com>
2024-09-01 10:42:27 +0530

    qwerert

--- e-commerce.py
+++ e-commerce.py
@@ -26,6 +26,16 @@ def admin():
     return jsonify([{'order_id': row[0], 'username': row[1], 'product_name': row[2]} for row in orders])
 
 
+@app.route('/admin', methods=['GET'])
+def admin():
+    db = get_db()
+    orders = db.execute('''SELECT o.id, u.username, p.name 
+                           FROM orders om
+                           JOIN users u ON o.user_id = u.id 
+                           JOIN products p ON o.product_id = p.id''').fetchall()
+    return jsonify([{'order_id': row[0], 'username': row[1], 'product_name': row[2]} for row in orders])
+
+
 # @app.route('/dummy', methods=['POST'])
 # def dummy():
 #     session.clear()
