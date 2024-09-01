08e6efc6aa9feb00cad42cea8e8295e6da84a78a
manish92596 <manishkumar92596@gmail.com>
2024-09-01 16:55:29 +0530

    admina

--- e-commerce.py
+++ e-commerce.py
@@ -26,14 +26,14 @@ app.secret_key = 'super_secure_secret_key'
 #     return jsonify([{'order_id': row[0], 'username': row[1], 'product_name': row[2]} for row in orders])
 
 
-# @app.route('/admin', methods=['GET'])
-# def admin():
-#     db = get_db()
-#     orders = db.execute('''SELECT o.id, u.username, p.name 
-#                            FROM orders om
-#                            JOIN users u ON o.user_id = u.id 
-#                            JOIN products p ON o.product_id = p.id''').fetchall()
-#     return jsonify([{'order_id': row[0], 'username': row[1], 'product_name': row[2]} for row in orders])
+@app.route('/admin', methods=['GET'])
+def admin():
+    db = get_db()
+    orders = db.execute('''SELECT o.id, u.username, p.name 
+                           FROM orders om
+                           JOIN users u ON o.user_id = u.id 
+                           JOIN products p ON o.product_id = p.id''').fetchall()
+    return jsonify([{'order_id': row[0], 'username': row[1], 'product_name': row[2]} for row in orders])
 
 
 @app.route('/dummy', methods=['POST'])
